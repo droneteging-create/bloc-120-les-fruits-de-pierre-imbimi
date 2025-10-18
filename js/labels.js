0b54103a6ab64f64f998935f46b058df17f39081
droneteging-create <droneteg.ing@gmail.com>
2025-10-18 07:27:10 +0200

    Add files via upload

--- js/labels.js
+++ js/labels.js
@@ -25,38 +25,48 @@ function resetLabels(markers) {
 
 function addLabel(layer, id) {
 
-  // This is ugly but there is no getContainer method on the tooltip :(
-  if (layer.getTooltip()) {
-      var label = layer.getTooltip()._source._tooltip._container;
-      if (label) {
+    var tooltip = layer.getTooltip();
+    if (!tooltip) {
+        return;
+    }
+    // This is ugly but there is no getContainer method on the tooltip :(
+    var label = tooltip._source._tooltip._container;
+    if (!label) {
+        return;
+    }
 
-        // We need the bounding rectangle of the label itself
-        var rect = label.getBoundingClientRect();
+    // If layer is hidden, hide its label and don't add to labelgun
+    if (layer.options.opacity === 0) {
+        label.style.opacity = 0;
+        label.style.transition = 'opacity 0s';
+        return;
+    }
 
-        // We convert the container coordinates (screen space) to Lat/lng
-        var bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
-        var topRight = map.containerPointToLatLng([rect.right, rect.top]);
-        var boundingBox = {
-          bottomLeft : [bottomLeft.lng, bottomLeft.lat],
-          topRight   : [topRight.lng, topRight.lat]
-        };
+    // We need the bounding rectangle of the label itself
+    var rect = label.getBoundingClientRect();
 
-        // Ingest the label into labelgun itself
-        labelEngine.ingestLabel(
-          boundingBox,
-          id,
-          parseInt(Math.random() * (5 - 1) + 1), // Weight
-          label,
-          "Test " + id,
-          false
-        );
+    // We convert the container coordinates (screen space) to Lat/lng
+    var bottomLeft = map.containerPointToLatLng([rect.left, rect.bottom]);
+    var topRight = map.containerPointToLatLng([rect.right, rect.top]);
+    var boundingBox = {
+        bottomLeft : [bottomLeft.lng, bottomLeft.lat],
+        topRight   : [topRight.lng, topRight.lat]
+    };
 
-        // If the label hasn't been added to the map already
-        // add it and set the added flag to true
-        if (!layer.added) {
-          layer.addTo(map);
-          layer.added = true;
-        }
-      }
-  }
+    // Ingest the label into labelgun itself
+    labelEngine.ingestLabel(
+        boundingBox,
+        id,
+        parseInt(Math.random() * (5 - 1) + 1), // Weight
+        label,
+        "Test " + id,
+        false
+    );
+
+    // If the label hasn't been added to the map already
+    // add it and set the added flag to true
+    if (!layer.added) {
+        layer.addTo(map);
+        layer.added = true;
+    }
 }
